37a3e055b2bf5fb5a9d304a15441410b7d61704e
Ben Wilson <benjamin.j.wilson1990@gmail.com>
2019-09-07 11:08:50 -0500

    react working

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use(routes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/reactreadinglist", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+// MongoDB Connection Error
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// MongoDB Connection Success
+db.on("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// API Server start
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
