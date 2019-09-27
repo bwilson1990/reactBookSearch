ac29e63bc5d0700e7fb30c62982aa8580496c17b
Ben Wilson <benjamin.j.wilson1990@gmail.com>
2019-09-26 20:42:01 -0500

    run build for heroku

---
+++ client/build/precache-manifest.84906f675e349882a2f426c6a17b11c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2fbb98d85f1cb5c492f631a8eb076b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "077858fdfcad806dbdd0",
+    "url": "/static/css/main.74edd691.chunk.css"
+  },
+  {
+    "revision": "a694c99e646f45de3d23",
+    "url": "/static/js/2.179b229a.chunk.js"
+  },
+  {
+    "revision": "077858fdfcad806dbdd0",
+    "url": "/static/js/main.7421045d.chunk.js"
+  },
+  {
+    "revision": "59c6c0646e2b82dfe4a7",
+    "url": "/static/js/runtime~main.3ea4f187.js"
+  }
+]);
